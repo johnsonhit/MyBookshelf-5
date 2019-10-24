6b155498a62d8e91dda1c87948129be551a8e733
Jonon <mochathecoder@gmail.com>
2019-10-23 22:14:43 -0700

    Tabbar added with view controllers

---
+++ SendBird/Controllers/NewBooksVC.swift
@@ -0,0 +1,27 @@
+//
+//  NewBooksVC.swift
+//  SendBird
+//
+//  Created by Jonathan on 10/23/19.
+//  Copyright © 2019 Jonathan. All rights reserved.
+//
+
+import UIKit
+
+class NewBooksVC: UIViewController {
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        title = "New"
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ SendBird/Controllers/SearchBooksVC.swift
@@ -0,0 +1,38 @@
+//
+//  SearchBooksVC.swift
+//  SendBird
+//
+//  Created by Jonathan on 10/23/19.
+//  Copyright © 2019 Jonathan. All rights reserved.
+//
+
+import UIKit
+
+class SearchBooksVC: UIViewController {
+
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        title = "Search"
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
