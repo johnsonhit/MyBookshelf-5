a559016af0b8ec7c0cbe460393cbad7ecd538e30
Jonon <mochathecoder@gmail.com>
2019-10-28 17:51:29 -0700

    Unit test NewBooks Response

--- SendBirdTests/ResponseTests/NewBooks/NewBooksResponseTest.swift
+++ SendBirdTests/ResponseTests/NewBooks/NewBooksResponseTest.swift
@@ -14,7 +14,21 @@ class NewBooksResponseTest: XCTestCase {
     func testNewBooksResponse() {
         let jsonData = grabJsonData(for: "MockNewBooksResponse")
         let newBooksResponse = try! JSONDecoder().decode(NewBooksResponse.self, from: jsonData)
-        XCTAssertNotNil(newBooksResponse)
+        let totalBooks = newBooksResponse.total
+        XCTAssertEqual(totalBooks, 2)
+        let books = newBooksResponse.books
+        XCTAssertEqual(books.count, 2)
+        let designingSensesBook = books.first!
+        let expectedTitle = "Designing Across Senses"
+        XCTAssertEqual(designingSensesBook.title, expectedTitle)
+        let expectedSubTitle = "A Multimodal Approach to Product Design"
+        XCTAssertEqual(designingSensesBook.subtitle, expectedSubTitle)
+        let expectedISBN13 = "9781491954249"
+        XCTAssertEqual(designingSensesBook.isbn13, expectedISBN13)
+        let expectedImageString = "https://itbook.store/img/books/9781491954249.png"
+        XCTAssertEqual(designingSensesBook.image, expectedImageString)
+        let expectedURLString = "https://itbook.store/books/9781491954249"
+        XCTAssertEqual(designingSensesBook.url, expectedURLString)
     }
 
 }
